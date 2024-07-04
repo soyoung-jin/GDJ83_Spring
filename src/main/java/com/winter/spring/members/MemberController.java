420bfd491052ea9dbe546bb46c39767478a6c3e3
soyoung-jin <jthdud0501@gmail.com>
2024-07-04 17:35:58 +0900

    2024.07.04

--- src/main/java/com/winter/spring/members/MemberController.java
+++ src/main/java/com/winter/spring/members/MemberController.java
@@ -1,11 +1,14 @@
 package com.winter.spring.members;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -28,7 +31,9 @@ public class MemberController {
 	}
 
 	@RequestMapping(value = "login", method = RequestMethod.GET) // 기억하고 있는 쿠키를 출력해서 jsp로 보냄
-	public void login() throws Exception {
+	public void login(HttpServletRequest request, Model model,
+			@CookieValue(name = "remember", required = false, defaultValue = "") String value) {
+		model.addAttribute("id", value);
 
 	}
 //	@RequestMapping(value = "login", method = RequestMethod.GET) // 기억하고 있는 쿠키를 출력해서 jsp로 보냄
@@ -56,7 +61,7 @@ public class MemberController {
 			response.addCookie(cookie);
 		} else {
 			Cookie cookie = new Cookie("remember", "");
-			cookie.setMaxAge(60);
+			cookie.setMaxAge(0);
 			response.addCookie(cookie);
 		}
 
@@ -68,6 +73,13 @@ public class MemberController {
 		}
 
 		return "redirect:/";
+	}
+
+	@RequestMapping(value = "logout", method = RequestMethod.GET)
+	public String logout(HttpSession session) throws Exception {
+		session.invalidate(); // session의 유지시간을 0으로
+		return "redirect:/";
+
 	}
 
 }
--- src/main/webapp/WEB-INF/views/sample/header.jsp
+++ src/main/webapp/WEB-INF/views/sample/header.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
 	 <ul class="nav">
 	  <li class="nav-item">
@@ -11,11 +12,47 @@
 	  <li class="nav-item">
 	    <a class="nav-link" href="#">Link</a>
 	  </li>
+	  
+	  <c:choose>
+	  	<c:when test="${not empty member}">
+		   <li class="nav-item">
+		    <a class="nav-link" href="/member/logout">로그아웃</a>
+		  </li>
+		  	  <li class="nav-item">
+		    <a class="nav-link" href="/member/mypage">마이페이지</a>
+		  </li>
+	  	</c:when>
+	  
+	  	<c:otherwise> 
+			<li class="nav-item">
+				  <a class="nav-link" href="/member/join">회원가입</a>
+			</li>
+				  <li class="nav-item">
+			<a class="nav-link" href="/member/login">로그인</a>
+			</li>
+		  </c:otherwise>
+	  </c:choose>
+	  
+	  
+	  <%-- <c:if test="${empty member}} }">
 	  <li class="nav-item">
 	    <a class="nav-link" href="/member/join">회원가입</a>
 	  </li>
 	  	  <li class="nav-item">
 	    <a class="nav-link" href="/member/login">로그인</a>
 	  </li>
+	  </c:if>
+	  
+	  
+	  <c:if test="${not empty member} }">
+	   <li class="nav-item">
+	    <a class="nav-link" href="/member/logout">로그아웃</a>
+	  </li>
+	  	  <li class="nav-item">
+	    <a class="nav-link" href="/member/mypage">마이페이지</a>
+	  </li>
+	  
+	  </c:if>--%>
+	  
 	</ul>
 </nav>
